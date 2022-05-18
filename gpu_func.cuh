c43902d8544d16c456ebef134cbe0b58cfa6af6d
Jyd <2275519762@qq.com>
2022-05-18 15:16:23 +0800

    Update gpu_func.cuh

--- gpu_func.cuh
+++ gpu_func.cuh
@@ -6,6 +6,8 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+typedef unsigned char uchar;
+
 __device__ void dev_sigmoid(float *ptr, int len);
 __global__ void process_kernel(float *dev_ptr, int height, int width, int no, int total_pix);  // , float* anchors
 
